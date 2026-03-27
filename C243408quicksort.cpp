f21ef4d88604556c9e310fd4b71c0e41d17b962e
shanjida123 <shanjidatalukder99@gmail.com>
2026-03-27 19:48:57 +0600

    Add files via upload

---
+++ C243408quicksort.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = 5;
+
+    cout << "Before sorting: ";
+    printArray(arr, n);
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "After sorting: ";
+    printArray(arr, n);
+
+    return 0;
+}
