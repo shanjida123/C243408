ebcf1b059ad72638ee5f0318accea2478158e28a
shanjida123 <shanjidatalukder99@gmail.com>
2026-05-01 20:50:02 +0600

    Add files via upload

---
+++ knapsack(dp).cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int main() {
+    int n, W;
+    cin >> n >> W;
+
+    int val[n], wt[n];
+    for(int i = 0; i < n; i++)
+        cin >> val[i] >> wt[i];
+
+    int dp[n+1][W+1];
+
+    for(int i = 0; i <= n; i++) {
+        for(int w = 0; w <= W; w++) {
+            if(i == 0 || w == 0)
+                dp[i][w] = 0;
+            else if(wt[i-1] <= w)
+                dp[i][w] = max(dp[i-1][w],
+                               val[i-1] + dp[i-1][w - wt[i-1]]);
+            else
+                dp[i][w] = dp[i-1][w];
+        }
+    }
+
+    cout << dp[n][W];
+}
